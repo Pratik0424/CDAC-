d94e35f92c9d776141fb03482eb411fb86cdd655
KUMAVAT PRATIK <159470401+Pratik0424@users.noreply.github.com>
2024-10-29 00:48:46 +0530

    Create imp.java

---
+++ InterView/imp.java
@@ -0,0 +1,12 @@
+
+//1. Check if a number is EVEN or ODD
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 4; 
+        if (num % 2 == 0) 
+            System.out.println(num + " is Even");
+        else 
+            System.out.println(num + " is Odd");
+    }
+}
